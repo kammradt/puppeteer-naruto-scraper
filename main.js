0cb49cfe14f6c61c212c2d43f4c467d83a0fc443
Vinicius Kammradt <vinicius.kammradt1@gmail.com>
2019-12-24 22:44:58 -0300

    :art: Refactoring ES6 code to look better and be easier to read

--- main.js
+++ main.js
@@ -11,12 +11,9 @@ const puppeteer = require('puppeteer');
   let characters = []
   for (let i = 0; i < links.length; i++) {
     await page.goto(links[i].url);
-    console.log(await page.evaluate(() => document.location.href))
     characters = [...characters, ...await getCharactersFromBiographyPage(page)]
   }
-
-  characters.forEach(c => console.log(c))
-
+  
   browser.close()
 })()
 
@@ -24,24 +21,35 @@ async function getCharactersFromBiographyPage(page) {
   return await page.evaluate(() => {
     let names = Array.from(document.querySelectorAll('[bgcolor="8AB275"]'))
     let images = Array.from(document.querySelectorAll('[width="15%"]'))
-      .filter(tableContainer => Array.from(tableContainer.children).length > 0 ? tableContainer.children[0].nodeName.toLowerCase() === "img" : false)
-      .map(rawIimage => Array.from(rawIimage.querySelectorAll('[border="1"]'))[0].src)
-  return names
-    .filter(name => name)
-    .map((name, i) => {
-      return {
+      .filter(tableContainer => {
+        let containerElements = Array.from(tableContainer.children)
+        if (containerElements.length > 0) {
+          let possibleImageTag = containerElements[0]
+          return possibleImageTag.nodeName.toLowerCase() === "img"
+        }
+      })
+      .map(rawIimage => {
+        let imageTag = Array.from(rawIimage.querySelectorAll('[border="1"]'))[0]
+        return imageTag.src
+      })
+
+    return names
+      .filter(validName => validName)
+      .map((name, i) => ({
         image: images[i],
         name: name.innerText
           .replace('[Click for Full Biography (Spoilers)]', '')
           .replace('�', '')
-      }
-    })
-})
+      })
+      )
+  })
 }
 
 async function getAllBiographyLinks(page) {
-  return await page.evaluate(() =>
-    Array.from(document.getElementsByTagName(`center`)[1].getElementsByTagName(`a`))
-      .map(biographyLink => ({ url: biographyLink.href }))
-  );
+  return await page.evaluate(() => {
+    let tableWithLinks = Array.from(document.getElementsByTagName(`center`)[1].getElementsByTagName(`a`))
+    return tableWithLinks.map(biographyLink => ({
+      url: biographyLink.href
+    }))
+  });
 }
